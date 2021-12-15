e792bddd79a0036c5002fcd26d6315b3845d5146
Рустам <leofoxi73@gmail.com>
2021-12-15 16:37:32 +0400

    Игнор

--- core/protocol/controller/controller.c
+++ core/protocol/controller/controller.c
@@ -18,37 +18,37 @@ int MAIN_CONTROLLER(WorkTable * ram){
     ram->actual_status_time_duration = clock()/CLOCKS_PER_SEC - ram->start_status_time;
 
     switch (ram->Status) {
-        //------------------------------------------------------------ 0
+        //------------------------------------------------------------
         case SLEEP:
             if(isTimeout(ram, DELAY_OF_SLEEP))
                 ram->Status = START_DEFINING_ROUTERS;
             break;
-        //------------------------------------------------------------ 1
+        //------------------------------------------------------------
         case START_DEFINING_ROUTERS:
             if(isTimeout(ram, DELAY_OF_START_DEFINING_ROUTERS))
                 ram->Status = ROUTER_IS_DEFINED;
             break;
-        //------------------------------------------------------------ 2
+        //------------------------------------------------------------
         case ROUTER_IS_DEFINED:
             if(isTimeout(ram, DELAY_OF_ROUTER_IS_DEFINED))
                 ram->Status = CONFIRM_FROM_POTENTIAL_ROUTER;
             break;
-        //------------------------------------------------------------ 3
+        //------------------------------------------------------------
         case CONFIRM_FROM_POTENTIAL_ROUTER:
             if(isTimeout(ram, DELAY_OF_CONFIRM_FROM_POTENTIAL_ROUTER))
                 ram->Status = ANNOUNCEMENT_POTENTIAL_ROUTER_STATUS;
             break;
-        //------------------------------------------------------------ 4
+        //------------------------------------------------------------
         case ANNOUNCEMENT_POTENTIAL_ROUTER_STATUS:
             if(isTimeout(ram, DELAY_OF_ANNOUNCEMENT_POTENTIAL_ROUTER_STATUS))
                 ram->Status = WAITING_CONFIRM_ROUTER_STATUS_FROM_DEVICES;
             break;
-        //------------------------------------------------------------ 5
+        //------------------------------------------------------------
         case WAITING_CONFIRM_ROUTER_STATUS_FROM_DEVICES:
             if(isTimeout(ram, DELAY_OF_WAITING_CONFIRM_ROUTER_STATUS_FROM_DEVICES))
                 ram->Status = ANNOUNCEMENT_ROUTER_STATUS;
             break;
-        //------------------------------------------------------------ 6
+        //------------------------------------------------------------
         case ANNOUNCEMENT_ROUTER_STATUS:
             if(isTimeout(ram, DELAY_OF_ANNOUNCEMENT_ROUTER_STATUS))
                 ram->Status = ADDITIONAL_WAITING_CONFIRM_ROUTER_STATUS_FROM_DEVICES;
