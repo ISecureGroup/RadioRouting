4a9841378782188233d3afc57262d183cdcbbe5e
Рустам <leofoxi73@gmail.com>
2022-02-15 13:32:26 +0400

    Старт с нуля

--- ardusketch/controller.hh
+++
@@ -1,8 +0,0 @@
-#ifndef   CONTROLLER_HH
-#define   CONTROLLER_HH
-#include  "protocol.hh"
-
-void MAIN_CONTROLLER(WorkTable * ram);                                                                           //УПРАВЛЯЮЩАЯ ЛОГИКА ПРОГРАММЫ
-void DefiningRouters(WorkTable *ram);                                                                            //ОПРЕДЕЛЕНИЕ ОСНОВНОГО И РЕЗЕРВНОГО РОУТЕРА
-
-#endif //CONTROLLER_H
